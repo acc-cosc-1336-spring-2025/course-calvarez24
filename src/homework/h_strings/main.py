e9943661d0dc99df92f06746b3646404e33be869
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-03-30 01:22:02 +0000

    hw 4

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,23 @@
-#
+
+def main():
+        while True:
+            print("\n1-Hamming Distance")
+            print("2-DNA Complement")
+            print("3-Exit")
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                dna1 = input("Enter the first DNA string: ")
+                dna2 = input("Enter the second DNA string: ")
+                print(f"Hamming Distance: {get_hamming_distance(dna1, dna2)}")
+            elif choice == '2':
+                dna = input("Enter the DNA string: ")
+                print(f"DNA Complement: {get_dna_complement(dna)}")
+            elif choice == '3':
+                print("Exiting...")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+        main()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,19 @@
-#
+def get_hamming_distance(dna1, dna2):
+    distance = 0
+    for i in range(len(dna1)):
+            if dna1[i] != dna2[i]:
+                distance += 1
+    return distance
+
+def get_dna_complement(dna):
+        complement = ""
+        for base in dna:
+            if base == 'A':
+                complement += 'T'
+            elif base == 'T':
+                complement += 'A'
+            elif base == 'C':
+                complement += 'G'
+            elif base == 'G':
+                complement += 'C'
+        return complement[::-1]  
