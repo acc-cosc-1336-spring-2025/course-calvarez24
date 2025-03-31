521ccee12447d7feb57b581c380440e32d076a61
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-03-31 21:45:04 +0000

    create an iterate array

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,14 @@
+import array #python library, not in the core python language 
+
 def test_config():
     return True
+
+def create_array1():
+    nums = array.array('i' , [1,2,3,4,5])  #creates an array list of integers, hence the 'i'
+    print(nums)  #
+
+def create_array2():
+    nums = array.array('i' , [1,2,3,4,5])
+
+    for num in nums:
+        print(num)
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,6 @@
-#main program
+import lists
+
+def main():
+    lists.create_array1() 
+    lists.create_array2()
+main()
