bed39ae36324e1c7da2252906d9ec995c9c98e4e
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-04-04 20:54:40 +0000

    hw 5

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,1 +1,19 @@
-#
+def get_lowest_list_value(value_list):
+    lowest_value = float('inf')
+
+    for value in value_list:
+          if value < lowest_value:
+                lowest_value = value
+
+    return lowest_value
+
+
+def get_highest_list_value(value_list):
+    highest_value = float('-inf')
+
+    for value in value_list:
+        if value > highest_value:
+                highest_value = value 
+
+    return highest_value
+
--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,10 +1,17 @@
 import unittest
 
-from src.examples.g_lists_and_tuples.lists import get_total_of_array_elements, test_config
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value , get_highest_list_value
 
 class Test_Config(unittest.TestCase):
 
-    def test_configuration(self):
-        self.assertEqual(True, test_config())
+    def test_get_lowest_value(self):
+        test_list = [8, 10, 1, 50, 20]
+        result = get_lowest_list_value(test_list)
 
- 
+        self.assertEqual(result, 1)
+
+    def test_get_highest_list_value(self):
+        test_list = [8, 10, 1, 50, 20]
+        result = get_highest_list_value(test_list)
+
+        self.assertEqual(result, 50)
