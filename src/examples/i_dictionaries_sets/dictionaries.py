f39c5a258eaef31fd172a9667759ce716889d8b9
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-04-16 01:00:48 +0000

    create/access values w keys dictionaries

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,2 +1,11 @@
 def test_config():
     return True
+
+def use_a_dictionary():
+    phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Jack'}
+    print(phonebook) #lazy print
+
+    print(phonebook['555-2222']) #finds the value, outputs Katie, keys work as indexs
+    print(phonebook['555-1111'])
+    print(phonebook['555-3333'])
+    #print(phonebook['555-4444']) #not a key, we get a key error 
