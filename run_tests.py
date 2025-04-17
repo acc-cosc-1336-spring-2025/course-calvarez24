7ee926df7ffce7ef4a1ba0fcddc2f22f215d23cd
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-04-17 17:38:52 +0000

    test adding key/value

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
+from tests.examples.i_dictionaries_sets import tests_dictionaries_and_sets
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
+suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
 unittest.TextTestRunner(verbosity=2).run(suite)  
--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -5,7 +5,7 @@ def use_a_dictionary():
     phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Jack'}
     print(phonebook) #lazy print
 
-    print(phonebook['555-2222']) #finds the value, outputs Katie, keys work as indexs
+    print(phonebook['555-2222']) #finds the value, outputs Katie, keys work as index's
     print(phonebook['555-1111'])
     print(phonebook['555-3333'])
 
@@ -13,3 +13,16 @@ def use_a_dictionary():
         print(phonebook['555-4444']) 
     else:
         print('Key does not exist')
+
+def add_key_value_pair():
+    phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Jack'}
+
+    key = input("Enter the key: ")
+    value = input("Enter the value: ")
+
+    if key not in phonebook:
+        phonebook[key] = value
+    else:
+        print(key, " already exists.")
+
+    print(phonebook)
