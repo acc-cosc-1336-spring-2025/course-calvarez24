46647770bce62155886f684ab31f817b491d655b
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-04-20 00:09:55 +0000

    hw 7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,31 @@
-#
+from dictionary import add_inventory , remove_inventory_widget
+
+def main():
+    widgets = {}
+
+    while True:
+        print("\nInventory Menu")
+        print("1- Add or Update Item")
+        print("2- Delete Item")
+        print("3- Exit")
+        choice = input("Enter choice (1-3): ")
+
+        if choice == "1":
+            name = input("Enter widget name: ")
+            try:
+                quantity = int(input("Enter quantity to add or update: "))
+                add_inventory(widgets, name, quantity)
+                print(f"{name} updated. Current quantity: {widgets[name]}")
+            except ValueError:
+                print("Please enter a valid integer for a quantity: ")
+        elif choice == "2":
+            name = input("Enter widget name to delete: ")
+            result = remove_inventory_widget(widgets, name)
+            print(result)
+        elif choice == "3":
+            print("Exiting the program...")
+            break 
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+main()
