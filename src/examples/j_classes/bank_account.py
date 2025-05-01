caea4e4be3072d3e40c286cce67749c48d786f01
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-05-01 23:33:07 +0000

    class bank account deposit

--- src/examples/j_classes/bank_account.py
+++ src/examples/j_classes/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
 
-    __balance = 100 #class variable-attributes-class data PRIVATE
+    __balance = 0 #class variable-attributes-class data __ means its private, needs to be protected
 
     #constructor -- special function
     def __init__(self, balance):
@@ -8,3 +8,8 @@ class BankAccount:
 
     def get_balance(self):
         return self.__balance 
+    
+    def deposit(self, amt):
+        if(amt > 0):
+            self.__balance += amt
+            
